d81ebb050065c36221fea9d286d33a0dbe3f848f
EgorFedotov <egorfedotovarz@gmail.com>
2023-04-26 12:51:09 +0300

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# converter
- Конвертаор валют
+## Конвертор валют
+
+## Стек
+
+- Python
+
+- Djando
+
+- API
+
+## Функционал сервиса
+
+-  Осуществляет конвертацию валют в режиме онлайн, Обмен валюты осуществляется по курсу http://www.exchangerate.com/
+
+
+## Запуск проекта
+
+Клонируем репозиторий к себе на ПК
+
+```bash
+  git clone git@github.com:EgorFedotov/converter.git
+```
+
+Переходим в дерикторию с проектом
+
+```bash
+  cd converter
+```
+
+Устанавливаем Виртуальное окружение
+
+```bash
+  py -3.7 -m venv venv
+```
+
+Активируем виртуальное окружение
+
+```bash
+  source venv/Scripts/activate
+```
+
+Устанавливаем зависимости
+
+```bash
+  pip install -r requirements.txt
+```
+
+переходим в папку app с файлом manage.py
+
+```bash
+  cd app
+```
+
+выполнгяем миграции:
+
+```bash
+  python python manage.py migrate
+```
+
+запускаем сервер
+
+```bash
+  python manage.py runserver
+```
--- app/exchenge/views.py
+++ app/exchenge/views.py
@@ -8,11 +8,9 @@ def exchange(request):
     currencies = response.get('rates')
 
     if request.method == 'GET':
-
         context = {
             'currencies': currencies
         }
-
         return render(
             request=request,
             template_name='exchenge/index.html',
@@ -23,11 +21,9 @@ def exchange(request):
         from_amount = float(request.POST.get('from-amount'))
         from_curr = request.POST.get('from-curr')
         to_curr = request.POST.get('to-curr')
-
         converted_amount = round(
             (currencies[to_curr] / currencies[from_curr])
             * float(from_amount), 2)
-
         context = {
             'from_curr': from_curr,
             'to_curr': to_curr,
@@ -35,5 +31,8 @@ def exchange(request):
             'currencies': currencies,
             'converted_amount': converted_amount
         }
-
-        return render(request=request, template_name='exchenge/index.html', context=context)
+        return render(
+            request=request,
+            template_name='exchenge/index.html',
+            context=context
+        )
